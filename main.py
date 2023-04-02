b37a2bc0ab75de64c59329600524b46cf2624db3
bendikbakken <bendiksbakken@gmail.com>
2023-04-02 21:04:35 +0200

    Fungerer på en måte. Mulig flere fortegnsfeil som nuller ut hverandre. Fortsatt masse rot

--- main.py
+++ main.py
@@ -1,6 +1,46 @@
+import panel_mehod as pm
+import numpy as np
+from matplotlib import pyplot as plt
 
 def main():
     print('Hello')
+    Q = 1
+    alpha_deg = 15
+    alpha_rad = alpha_deg*np.pi/180
+    panels_x, panels_z, circ_x, circ_z, colloc_x, colloc_z, alpha_i = pm.make_panels(pm.camber, 5, 1)
+    A, b = pm.calc_coeff(panels_x, panels_z, circ_x, circ_z, colloc_x, colloc_z, alpha_i, 1, alpha_rad)
+
+    gamma = np.linalg.solve(A, b)
+    print(gamma)
+
+    def vortex(gamma0, x, z, x0, z0):
+        return gamma0/(2*np.pi) * np.log(np.sqrt((x-x0)**2 + (z-z0)**2))
+        #return gamma0/(2*np.pi) * np.arctan2(z-z0, x-x0)
+
+    def stream(x, z, Q, alpha):
+        p = np.zeros(shape=np.shape(x))
+        p = Q * (z*np.cos(alpha) - x*np.sin(alpha))
+        for g, x0, z0 in zip(gamma, circ_x, circ_z):
+            p += vortex(g, x, z, x0, z0)
+        return p
+
+    X = np.linspace(-0.5, 1.5)
+    Z = np.linspace(-1, 1)
+    xg, zg = np.meshgrid(X, Z)
+
+    psi = stream(xg, zg, Q, alpha_rad)
+
+    x_foil = np.linspace(0,1)
+    z_foil = pm.camber(x_foil)
+    #plt.plot(x_foil, z_foil, linewidth=5, color='k')
+    plt.scatter(panels_x, panels_z)
+    plt.scatter(circ_x, circ_z, color='r')
+    plt.scatter(colloc_x, colloc_z, color='g')
+
+    plt.contour(xg, zg, psi, 30)
+    plt.show()
+
+
 
 if __name__ == '__main__':
     main()
--- panel_mehod.py
+++ panel_mehod.py
@@ -2,34 +2,40 @@ import numpy as np
 
 
 def camber(x):
-    if x <= 0.4:
-        z = 0.25 * (0.8 * x - x ** 2)
-    else:
-        z = 0.111 * (0.2 + 0.8 * x - x ** 2)
+    z = np.zeros(np.size(x))
+    for i in range(len(x)):
+        if x[i] <= 0.4:
+            z[i] = 0.25 * (0.8 * x[i] - x[i] ** 2)
+        else:
+            z[i] = 0.111 * (0.2 + 0.8 * x[i] - x[i] ** 2)
+    return z
 
 
 def make_panels(func, n, c):
     panels_x = np.linspace(0, c, n+1)
-    panels_z = camber(panels_x)
-    circ_points = np.interp(panels_x, panels_z, panels_x[:-1] + 1 / 4 * c / n)
-    colloc_points = np.interp(panels_x, panels_z, panels_x[:-1] + 3 / 4 * c / n)
-    alpha_i = np.arctan2(panels_z[1:]-panels_z[0:-1], panels_x[1:]-panels_x[0:-1])
+    panels_z = func(panels_x)
+    circ_x = panels_x[:-1] + 1 / 4 * c / n
+    colloc_x = panels_x[:-1] + 3 / 4 * c / n
+    circ_z = np.interp(circ_x, panels_x, panels_z)
+    colloc_z = np.interp(colloc_x, panels_x, panels_z)
+    alpha_i = -np.arctan2(panels_z[1:]-panels_z[:-1], panels_x[1:]-panels_x[:-1])
+    print(alpha_i)
+    return panels_x, panels_z, circ_x, circ_z, colloc_x, colloc_z, alpha_i
 
-
-def calc_freestream(Q, alpha):
-    return [Q*np.cos(alpha), Q*np.sin(alpha)]
-
-
-
-def calc_coeff(panels_x, panels_y, circ_points, colloc_points, alpha_i):
-    n = np.size(circ_points)
+def calc_coeff(panels_x, panels_z, circ_x, circ_z, colloc_x, colloc_z, alpha_i, Q, alpha):
+    n = np.size(circ_x)
     n_i = np.array([np.sin(alpha_i), np.cos(alpha_i)])
+    print(n_i)
     t_i = np.array([np.cos(alpha_i), -np.sin(alpha_i)])
-    a = np.zeros((n,n))
+    a = np.zeros((n, n))
+    M = np.array([[0,1],[-1,0]])
+    print(M)
 
-    uw = 1/(2*np.pi*()) * np
+    for j in range(n):
+        aj = (M @ np.array([(colloc_x-circ_x[j]), (colloc_z-circ_z[j])])) * (1/(2*np.pi) * 1/((colloc_x-circ_x[j])**2 + (colloc_z-circ_z[j])**2))
+        a[:,j] = np.array([a@n for a, n in zip(aj.T, n_i.T)])
 
-    def q(,)
-    for i in range(n):
-        for j in range(n):
-            a[i,j] = -1/(2*np.pi) * 1/r
+    b = -Q * np.sin(alpha + alpha_i)
+    print(b)
+    print(a)
+    return a, b
